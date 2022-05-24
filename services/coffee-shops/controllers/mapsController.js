f0ad4b004d166fe7c0f2df83a6ffa5a349a7d32b
Oktavicto <oktavicto@gmail.com>
2022-05-24 21:24:03 +0700

    clearing console log and fixing connection mongoDB

--- services/coffee-shops/controllers/mapsController.js
+++ services/coffee-shops/controllers/mapsController.js
@@ -7,18 +7,17 @@ class Controller {
       const nearby = await Maps.nearbySearch({ latitude, longitude });
       res.status(200).json(nearby);
     } catch (err) {
-      console.log(err);
       next(err);
     }
   }
 
   static async placeDetail(req, res, next) {
     try {
-      const {place_id} = req.query
-      const detail = await Maps.placeDetail(place_id)
-      res.status(200).json(detail)
+      const { place_id } = req.query;
+      const detail = await Maps.placeDetail(place_id);
+      res.status(200).json(detail);
     } catch (err) {
-      next(err)
+      next(err);
     }
   }
 }
